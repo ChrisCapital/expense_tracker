16c5b8a0541d491ceddfea24e069c6f81207efdc
ChrisCapital <chris.shavuo@gmail.com>
2026-03-26 11:46:05 -0400

    we installed uuid and created expense model

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
@@ -10,12 +12,26 @@ class Expenses extends StatefulWidget{
 }
 
 class _ExpensesState extends State<Expenses>{
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Ginos Pizza",
+      amount:25.00,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Amtrack Ticket",
+      amount:19.99,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
         children: [
-          Text("Chart"),
+          Text("Chart goes here..."),
           Text('Expenses List...'),
         ],
       ),
