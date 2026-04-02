8119848818a1ef6d41d23574b20d8aa2effd706a
ChrisCapital <chris.shavuo@gmail.com>
2026-04-02 11:56:57 -0400

    added text field, text controller and submit button

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,7 +1,7 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
-
+import 'package:expense_tracker/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
@@ -13,7 +13,11 @@ class Expenses extends StatefulWidget{
 }
 
 class _ExpensesState extends State<Expenses>{
-
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(
+      context:context,
+      builder: (ctx)=> NewExpense());
+  }
   final List<Expense> _registeredExpenses = [
     Expense(
       title: "Ginos Pizza",
@@ -31,15 +35,14 @@ class _ExpensesState extends State<Expenses>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: const Text("Expense Tracker"),
-        actions: [
-      IconButton(icon:const Icon(Icons.add),
-      onPressed:(){
-
-      },
-      )
-     ],
+  appBar:AppBar(
+    title: const Text("Expense Tracker"),
+    actions: [
+      IconButton(
+        icon: const Icon(Icons.add),
+        onPressed: _openAddExpenseOverlay,
+      ),
+    ],
   ),
   
       body: Column(
