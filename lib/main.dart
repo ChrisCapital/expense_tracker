8ee39ce85c7f61aa24ff619f3781adc0d1e84a30
ChrisCapital <chris.shavuo@gmail.com>
2026-04-16 11:51:20 -0400

    added chart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 ColorScheme kColorScheme = ColorScheme.fromSeed(
   seedColor:Color.fromARGB(255,96,59,181));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125)
+);
 void main() {
   runApp(
     MaterialApp(
@@ -29,6 +34,20 @@ void main() {
           ),
         ),
       ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal:16, vertical:8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer
+            ),
+          ),
+      ),
+      themeMode: ThemeMode.system,
       home: Expenses(),
     ),
   );
